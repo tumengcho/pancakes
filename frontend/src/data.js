8c26f2ed5aa7e0ea633b7dfe8e9c17ee15ba173d
Mbacke <tumengcho@gmail.com>
2023-07-12 19:03:57 -0400

    deployment 4

---
+++ frontend/src/data.js
@@ -0,0 +1,26 @@
+const data = {
+  packages: [
+    {
+      name: 'Basic Package',
+      price: 6.96,
+      description: 'The basic package with only 1 video that last 30 - 60secs.',
+      image: '/Images/b.png',
+    },
+
+    {
+      name: 'Standard Package',
+      price: 13.92,
+      description: ' The standard package with 2 videos that last 30 - 60secs.',
+      image: '/Images/s.png',
+    },
+
+    {
+      name: 'Premium Package',
+      price: 62.65,
+      description: ' The premium package with 4 videos that last 30-60secs.',
+      image: '/Images/p.png',
+    },
+  ],
+};
+
+export default data;
--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import data from '../data';
 
 export default function HomeScreen() {
   return (
@@ -30,13 +31,15 @@ export default function HomeScreen() {
       </Container>
       <Container>
         <Row className="align-items-center mb-5">
-          <Col md={6} className="order-md-0 order-1">
-            <h1 className="mb-4 px-5 pt-md-0 pt-sm-5 title ">Who am I?</h1>
-            <p className="px-5 desc">
+          <Col md={6} className="order-md-0 order-1 px-md-5">
+            <h5 className="pb-md-3 pt-3">ABOUT</h5>
+            <h1 className="mb-4 pt-xs-5 title ">Who am I?</h1>
+            <p className="desc1  mb-md-5">
               I am a decent/efficient Video Editor with over 2.5+ (2020-now)
               years of experience in video editing , but mostly been inspired
               early on in video editing.
             </p>
+            <button className="btn-section">Learn More</button>
           </Col>
           <Col md={6} className="order-md-1 order-0">
             <img
@@ -54,20 +57,27 @@ export default function HomeScreen() {
               alt="who-am-i"
             ></img>
           </Col>
-          <Col md={6} className="order-md-0 order-1 px-5">
-            <h1 className="mb-4 px-5 pt-md-0 pt-sm-5 title">Why choose me?</h1>
-            <p className="px-5 desc">
+          <Col md={6} className="order-md-0 order-1 px-md-5">
+            <h5 className="pb-md-3 ps-md-5 pt-3">ABOUT</h5>
+
+            <h1 className="mb-4 px-md-5 pt-md-0 pt-xs-5 title">
+              Why choose me?
+            </h1>
+            <p className="px-md-5 mb-4 desc">
               I am simple, friendly and professional. If there is any issue i
               deliver quick responses and I will always deliver on time.
             </p>
+            <button className=" ms-md-5 btn-section">Learn More</button>
           </Col>
         </Row>
         <Row className="align-items-center mt-5">
           <Col md={6} className="order-md-0 order-1">
-            <h1 className="mb-4 px-5 pt-md-0 pt-sm-5 title">
+            <h5 className="pb-md-3 ps-md-5 pt-3">ABOUT</h5>
+
+            <h1 className="mb-4 px-md-5 pt-md-0 pt-xs-5 title">
               How does the proccess work?
             </h1>
-            <p className="px-5 desc">
+            <p className="px-md-5 mb-4 desc">
               <strong>Step 1:</strong> Send your pre-cut videos. <br />
               <br />
               <strong>Step 2:</strong> Logo (optional).
@@ -76,6 +86,7 @@ export default function HomeScreen() {
               <strong>Step 3:</strong> Send Example of the style you want of the
               project (optional).
             </p>
+            <button className=" ms-md-5 btn-section">Learn More</button>
           </Col>
           <Col md={6} className="order-md-1 order-0">
             <img
@@ -85,6 +96,25 @@ export default function HomeScreen() {
             ></img>
           </Col>
         </Row>
+        <Container className="mt-5">
+          <Row>
+            {data.packages.map((product) => (
+              <Col md={4} className="text-center">
+                <div>
+                  <img
+                    className="img-package"
+                    src={product.image}
+                    alt={product.name}
+                  ></img>
+                  <h3 className="mt-3 fw-normal">{product.name}</h3>
+                  <p className="mt-3 fs-7 px-5 fw-light">
+                    {product.description}
+                  </p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </Container>
     </div>
   );
