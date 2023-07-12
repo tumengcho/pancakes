4177a239ef82e3f8803db5667a2f0f51efca34c6
Mbacke <tumengcho@gmail.com>
2023-07-12 04:40:15 -0400

    deployment

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import data from './data.js';
+import path from 'path';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import producRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoutes.js';
+import expressAsyncHandler from 'express-async-handler';
+import orderRouter from './routes/orderRoutes.js';
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(_dirname, '/frontend/build/index.html'))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
